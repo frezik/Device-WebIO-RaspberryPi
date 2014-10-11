096d84b0bb272b8307f4da6deb06f89db712dd30
Timm Murray <tmurray@wumpus-cave.net>
2014-10-11 18:23:04 -0500

    Correct how temperature is read

--- examples/tmp102.pl
+++ examples/tmp102.pl
@@ -6,6 +6,7 @@ use HiPi::Device::I2C ();
 use constant DEVICE        => 1;
 use constant SLAVE_ADDR    => 0x48;
 use constant TEMP_REGISTER => 0x00;
+use constant DEBUG         => 0;
 
 
 my $webio = Device::WebIO->new;
@@ -13,8 +14,43 @@ my $rpi   = Device::WebIO::RaspberryPi->new;
 $webio->register( 'rpi', $rpi );
 
 while( 1 ) {
-    my ($temp) = $webio->i2c_read( 'rpi',
-        DEVICE, SLAVE_ADDR, TEMP_REGISTER, 1 );
+    my ($temp1, $temp2) = $webio->i2c_read( 'rpi',
+        DEVICE, SLAVE_ADDR, TEMP_REGISTER, 2 );
+    my $temp = convert_reading( $temp1, $temp2 );
     say 'Temp: ' . $temp . 'C';
     sleep 1;
+}
+
+
+# Taken from Device::Temperature::TMP102
+sub convert_reading
+{
+    my ( $msb, $lsb ) = @_;
+
+    printf( "msb:     %02x\n", $msb )   if DEBUG;
+    printf( "lsb:     %02x\n", $lsb )   if DEBUG;
+
+    my $temp = ( $msb << 8 ) | $lsb;
+
+    # The TMP102 temperature registers are left justified, correctly
+    # right justify them
+    $temp = $temp >> 4;
+
+    # test for negative numbers
+    if ( $temp & ( 1 << 11 ) ) {
+
+    # twos compliment plus one, per the docs
+    $temp = ~$temp + 1;
+
+    # keep only our 12 bits
+    $temp &= 0xfff;
+
+    # negative
+    $temp *= -1;
+    }
+
+    # convert to a celsius temp value
+    $temp = $temp / 16;
+
+    return $temp;
 }
